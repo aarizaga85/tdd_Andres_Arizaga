6dc09344a3eb3f5ced1891511d1973311e3795e8
aarizaga85 <andresarizaga@gmail.com>
2023-05-18 16:10:26 -0500

    Actualización de practica

--- src/main/java/ec/edu/ups/tdd/calculator/ICalculator.java
+++ src/main/java/ec/edu/ups/tdd/calculator/ICalculator.java
@@ -6,4 +6,10 @@ package ec.edu.ups.tdd.calculator;
 public interface ICalculator {
 
     public int addition(int a, int b);
+
+    public int subtraction(int a, int b);
+
+    public int division(int a, int b);
+
+    public int multiplication(int a, int b);
 }
--- src/test/java/ec/edu/ups/tdd/calculator/ICalculatorTest.java
+++ src/test/java/ec/edu/ups/tdd/calculator/ICalculatorTest.java
@@ -6,12 +6,33 @@ import org.mockito.Mockito;
 import static org.junit.Assert.*;
 
 public class ICalculatorTest {
+    ICalculator c = Mockito.mock(ICalculator.class);
 
     @Test
     public void given_two_integers_when_addition_then_ok() {
-        ICalculator c = Mockito.mock(ICalculator.class);
         Mockito.when(c.addition(2, 3)).thenReturn(5);
 
         assertEquals(5, c.addition(2, 3));
+    }
+
+    @Test
+    public void given_two_integers_when_subtraction_then_ok(){
+        Mockito.when(c.subtraction(4, 2)).thenReturn(2);
+
+        assertEquals(2, c.subtraction(4, 2));
+    }
+
+    @Test
+    public void given_two_integers_when_division_then_ok(){
+        Mockito.when(c.division(6, 2)).thenReturn(3);
+
+        assertEquals(3, c.division(6, 2));
+    }
+
+    @Test
+    public void given_two_integers_when_multiplication_then_ok(){
+        Mockito.when(c.multiplication(4, 2)).thenReturn(8);
+
+        assertEquals(8, c.multiplication(4, 2));
     }
 }
